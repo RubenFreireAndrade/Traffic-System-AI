d0c416f367558c9bee8bad2a7279a63716f9cf9c
Ruben Freire Andrade <rubenswork20@gmail.com>
2022-12-31 16:13:32 +0000

    Moving files to Repo

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public GameObject Car;
+    public float speed = 5.0f;
+    public float safeDistance = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = new Ray(this.transform.position, this.transform.right);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, safeDistance))
+        {
+            var lastHit = hit.transform.gameObject;
+            if(lastHit.CompareTag("Car"))
+            {
+                Debug.Log("Car was hit");
+                Stop();
+            }
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+    }
+
+    private void Stop()
+    {
+        Car.transform.position += new Vector3(0, 0, 0);
+    }
+
+    private void Move()
+    {
+        Car.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
